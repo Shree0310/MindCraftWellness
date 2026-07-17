0ebd34cee7dc8053fe013339876acd34c0da7725
Sourashree <sourashree03@gmail.com>
2026-07-17 20:21:07 +0530

    Changes to book now page

--- src/app/book-now/page.tsx
+++ src/app/book-now/page.tsx
@@ -1,9 +1,5 @@
 import BookNow from '../Components/BookNow';
-import Footer from '../Components/Footer';
 
 export default function BookNowPage() {
-  return <div className="div">
-      <BookNow />;
-      <Footer/>
-  </div>
+  return <BookNow />;
 }
